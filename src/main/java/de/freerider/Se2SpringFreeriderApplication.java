2584a2458aca5009d605f4ac58f2f5c20dadd412
paulgro1 <68597696+paulgro1@users.noreply.github.com>
2021-06-04 16:15:10 +0200

    initial src commit

---
+++ src/main/java/de/freerider/Se2SpringFreeriderApplication.java
@@ -0,0 +1,13 @@
+package de.freerider;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Se2SpringFreeriderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Se2SpringFreeriderApplication.class, args);
+	}
+
+}
